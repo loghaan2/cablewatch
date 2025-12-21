9892277e1e952e10187c2226ed6857383de7a858
Sébastien MATZ <loghaan@benead.org>
2025-12-21 17:50:04 +0100

    clablewatch.config: initiate config module
    Signed-off-by: Sébastien MATZ <loghaan@benead.org>

---
+++ src/cablewatch/config.py
@@ -0,0 +1,61 @@
+import pathlib
+import re
+
+
+class Config:
+    _state = None
+
+    WEB_LISTENADDR = '0.0.0.0'
+    WEB_PORT = 8000
+    WEB_ROOTDIR = '{PROJECT_DIR}/www'
+
+    def __init__(self):
+        if self.__class__._state is not None:
+            self.__dict__ = self._state
+            return
+        self.PROJECT_DIR = str(pathlib.Path(__file__).parent.parent.parent)
+        self.__class__._state = self.__dict__
+
+    @staticmethod
+    def _is_conf_attr_name(name):
+        if (name.startswith('_')) or (name != name.upper()):
+            return False
+        else:
+            return True
+
+    def __getattribute__(self, name):
+        if Config._is_conf_attr_name(name):
+            return self._get_conf_attr(name)
+        else:
+            return super().__getattribute__(name)
+
+    def _get_conf_attr(self, name, *, resolve_context=None):
+        if resolve_context is None:
+            resolve_context = [name]
+        if len(resolve_context) > 8:
+            raise RecursionError(f'Recursion error while resolving {" -> ".join(resolve_context)}')
+        value = super().__getattribute__(name)
+        if isinstance(value, str):
+            d = {}
+            for k in re.findall(r"\{([\w]+)\}", value):
+                if Config._is_conf_attr_name(k):
+                    d[k] = self._get_conf_attr(k, resolve_context=resolve_context + [k])
+            return value.format(**d)
+        else:
+            return value
+
+    def asDict(self):
+        d = {}
+        for k in dir(self):
+            if Config._is_conf_attr_name(k):
+                v = self._get_conf_attr(k)
+                d[k] = v
+        return d
+
+    def __repr__(self):
+        rval = f'<{self.__class__.__name__}\n'
+        d = self.asDict()
+        for k,v in d.items():
+            rval += f'  {k}={v!r}\n'
+        rval += '>\n'
+        return rval
