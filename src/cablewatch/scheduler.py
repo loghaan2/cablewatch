debb874efeedaf799e468a5642720932fa8d4664
Sébastien MATZ <loghaan@benead.org>
2026-01-06 14:18:07 +0100

    cablewatch.scheduler: better stop() method and also fix some typos
    Signed-off-by: Sébastien MATZ <loghaan@benead.org>

--- src/cablewatch/scheduler.py
+++ src/cablewatch/scheduler.py
@@ -6,22 +6,24 @@ from cablewatch import config
 class SchedulerService:
     def __init__(self, ingest_service):
         self._ingest_service = ingest_service
+        self._sched = None
 
     async def start(self):
         conf = config.Config()
         sched = AsyncIOScheduler(timezone=conf.TIMEZONE)
-        logger.info("sheduler service starting")
+        logger.info("scheduler service starting")
         sched.add_job(self.record, trigger="cron", hour=6, minute=25)
         sched.add_job(self.halt, trigger="cron", hour=0, minute=5)
         sched.start()
         self._sched = sched
-        logger.info("sheduler service started")
+        logger.info("scheduler service started")
 
     async def stop(self):
-        logger.info("sheduler service stoping")
+        logger.info("scheduler service stoping")
         sched = self._sched
-        sched.shutdown()
-        logger.info("sheduler service stopped")
+        if sched is not None:
+            sched.shutdown()
+        logger.info("scheduler service stopped")
 
     def record(self):
         logger.warning("record requested by scheduler")
