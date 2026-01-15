59fe102418db36063c53252ae444bb53d997d744
Sébastien MATZ <loghaan@benead.org>
2026-01-15 14:20:17 +0100

    cablewatch.database: add some logs
    Signed-off-by: Sébastien MATZ <loghaan@benead.org>

--- src/cablewatch/database.py
+++ src/cablewatch/database.py
@@ -1,8 +1,10 @@
 import duckdb
+from loguru import logger
 from cablewatch import config
 
 
 def connect(read_only=False):
     conf = config.Config()
     path = f'{conf.DATABASE_PATH}'
+    logger.info(f"open database read_only={read_only}")
     return duckdb.connect(path, read_only=read_only)
